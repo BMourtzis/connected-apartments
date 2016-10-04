99a0decafd4ffd88e9e95f970fd9685b878f4fe3
BMourtzis <vasilis.mourtzis@outlook.com>
2016-10-04 12:44:45 +1100

    Added Reset Password functionality
    I added the reset functionality to the controller, including an email that includes a new password

--- ConnApsDomain/ConnApsContext.cs
+++ ConnApsDomain/ConnApsContext.cs
@@ -15,6 +15,7 @@ namespace ConnApsDomain
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Tenant>().HasRequired(t => t.Apartment).WithMany(a => a.Tenants).WillCascadeOnDelete(false);
+            modelBuilder.Entity<Booking>().HasRequired(b => b.Facility).WithMany(f => f.Bookings).WillCascadeOnDelete(false);
             modelBuilder.Entity<Building>().HasMany(b => b.Locations);
             modelBuilder.Entity<Building>().HasMany(b => b.Managers);
             modelBuilder.Entity<Apartment>().HasMany(a => a.Tenants);
