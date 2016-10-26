1f43ce4355d8dbe02fb7e207b292a82b19149a8c
BMourtzis <vasilis.mourtzis@outlook.com>
2016-10-26 19:17:11 +1100

    Changed the folder layout of testing and moved the test

---
+++ CommApsDomainUnitTest/WebAPI_Tests/TenantController_Tests/FetchTenantUnitTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xunit;
+using Moq;
+using ConnApsDomain;
+using ConnApsWebAPI.Controllers;
+
+namespace CommApsDomainUnitTest.WebAPI_Tests.TenantController_Tests
+{
+    public class FetchTenantUnitTest
+    {
+
+        [Fact]
+        public void TestMethod1()
+        {
+            //Assemble
+            var tenantFacade = new Mock<TenantFacade>();
+            var tenant = new Mock<ITenant>();
+            //tenantFacade.Setup(t => t.FetchTenant("")).Returns(tenant.Object);
+            //TenantController controller = new TenantController(tenantFacade.Object);
+
+            //Action
+
+
+            //Result
+            Assert.Equal(1, 1);
+
+        }
+    }
+}
